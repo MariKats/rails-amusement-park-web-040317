cdbafc5783eabc47e3f8ceb0b3b19b419de36220
github username <github email address>
2017-05-04 17:51:57 -0400

    passed the first test

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
 resources :users
 resources :attractions
 resources :rides
+resources :sessions
+root 'welcome#index'
+get '/signup', to: 'users#new', as: 'signup'
+get '/signin', to: 'sessions#create', as: 'signin'
 
 end
