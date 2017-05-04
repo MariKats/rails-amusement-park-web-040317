0e84e863d93a9ea1070be763ed7f59122c9cac17
github username <github email address>
2017-05-04 16:21:41 -0400

    we did the models

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,3 +1,29 @@
 class Ride < ActiveRecord::Base
-  # write associations here
+  belongs_to :user
+  belongs_to :attraction
+
+
+# has a method 'take_ride' that accounts for the user not having enough tickets (FAILED - 43)
+# has a method 'take_ride' that accounts for the user not being tall enough (FAILED - 44)
+# has a method 'take_ride' that accounts for the user not being tall enough and not having enough tickets (FAILED - 45)
+# has a method 'take_ride' that updates ticket number (FAILED - 46)
+# has a method 'take_ride' that updates the user's nausea (FAILED - 47)
+# has a method 'take_ride' that updates the user's happiness (FAILED - 48)
+
+  def take_ride
+    if self.user.tickets < self.attraction.tickets && self.user.height > self.attraction.min_height
+      "Sorry. You do not have enough tickets to ride the #{attraction.name}."
+    elsif self.user.height < self.attraction.min_height && self.user.tickets > self.attraction.tickets
+      "Sorry. You are not tall enough to ride the #{attraction.name}."
+    elsif self.user.tickets < self.attraction.tickets && self.user.height < self.attraction.min_height
+        "Sorry. You do not have enough tickets to ride the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
+    else
+      # binding.pry
+      self.user.tickets = self.user.tickets - self.attraction.tickets
+      self.user.nausea = self.user.nausea + self.attraction.nausea_rating
+      self.user.happiness = self.user.happiness + self.attraction.happiness_rating
+      self.user.save
+
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,19 @@
 class User < ActiveRecord::Base
-  # write associations here
+  has_secure_password
+  has_many :rides
+  has_many :attractions, through: :rides
+
+  def mood
+    if self.happiness > self.nausea
+      "happy"
+    else
+      "sad"
+    end 
+  end
+
+
+
 end
+
+# has a method 'mood' that returns 'sad' when the user is more nauseous than happy (FAILED - 43)
+#   has a method 'mood' that returns 'happy' when the user is more happy than nauseous (FAILED - 44)
